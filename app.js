015f2acbf8e09f9bf488bf918d48bf4a41682550
Jakub <jakubgalazka34@gmail.com>
2024-11-06 19:38:48 +0100

    naprawione

--- app.js
+++ app.js
@@ -1,52 +1,64 @@
-async function getData(){
-    const data = await fetch('https://restcountries.com/v3.1/region/europe')
-    const json = await data.json()
-    return json 
+async function getData() {
+    const data = await fetch('https://restcountries.com/v3.1/region/europe');
+    const json = await data.json();
+    return json;
 }
 
-getData()
-async function kraje(){
-    const countries  = await getData()
+async function kraje() {
+    const countries = await getData();
 
-    let body = document.querySelector('body')
-    let div = document.getElementById('div')
-    const input = document.querySelector('input')
-    let button = document.getElementById("button")
-    let country = Math.floor(Math.random()*countries.length)
-    let good = document.getElementById('good')
-    let bad = document.getElementById('bad')
-    let goodAnsw = 0
-    let badAnsw = 0
-    let img = document.createElement('img')
-    img.setAttribute("src", countries[country].flags.png)
-    div.appendChild(img)
-    let nazwa = document.createElement("h1")
-    nazwa.innerHTML=countries[country].name.common
-    div.appendChild(nazwa)
-    let p = document.createElement('p')
-    div.appendChild(p)
-    console.log(countries[country].capital[0]);
+    const div = document.getElementById('div');
+    const input = document.querySelector('input');
+    const button = document.getElementById("button");
+    const good = document.getElementById('good');
+    const bad = document.getElementById('bad');
+    let goodAnsw = 0;
+    let badAnsw = 0;
+    let country;
 
-    button.addEventListener('click', function(){
-        console.log(input.value);
-        if(input.value==countries[country].capital[0]){
-            p.innerHTML="Zgadłeś!"
-            goodAnsw = goodAnsw+1   
+    const img = document.createElement('img');
+    const nazwa = document.createElement("h1");
+    const p = document.createElement('p');
+
+    div.appendChild(img);
+    div.appendChild(nazwa);
+    div.appendChild(p);
+
+    function losujKraj() {
+        country = Math.floor(Math.random() * countries.length);
+        img.setAttribute("src", countries[country].flags.png);
+        nazwa.innerHTML = countries[country].name.common;
+        p.innerHTML = "";
+        input.value = "";
+    }
+    function nowaGra() {
+        goodAnsw = 0;
+        badAnsw = 0;
+        good.innerHTML = `Poprawne: ${goodAnsw}`;
+        bad.innerHTML = `Niepoprawne: ${badAnsw}`;
+        losujKraj();
+    }
+    nowaGra();
+
+    button.addEventListener('click', function() {
+        if (input.value === countries[country].capital[0]) {
+            p.innerHTML = "Zgadłeś!";
+            goodAnsw++;
+        } else {
+            p.innerHTML = "Nie zgadłeś";
+            badAnsw++;
         }
-        else if(input.value!=countries[country].capital[0]){
-            p.innerHTML="Nie zgadłeś"
-            badAnsw = badAnsw+1
+
+        good.innerHTML = `Poprawne: ${goodAnsw}`;
+        bad.innerHTML = `Niepoprawne: ${badAnsw}`;
+
+        if (badAnsw === 5) {
+            alert(`Zgadłeś ${goodAnsw} stolic. Nowa gra rozpocznie się automatycznie.`);
+            nowaGra();
+        } else {
+            losujKraj();
         }
-        good.innerHTML = `Poprawne:${goodAnsw}`
-        bad.innerHTML = `Niepoprawne:${badAnsw}`
-        if(badAnsw==5){
-            alert(`Zgadłeś ${goodAnsw} stolic`)
-        }
-        else{
-            img.setAttribute("src", countries[country].flags.png)
-            nazwa.innerHTML=countries[country].name.common
-            kraje()
-        }
-    })
+    });
 }
-kraje()
+
+kraje();
